b7feec4ba0397fb8057b2a92d788de239e3f80dc
roeliars <roman@fev.com.mx>
2022-03-23 21:43:20 -0600

    Se agrego otra opción de color

--- Paint.py
+++ Paint.py
@@ -1,15 +1,30 @@
+"""Paint, for drawing shapes.
+
+Exercises
+
+1. Add a color.
+2. Complete circle.
+3. Complete rectangle.
+4. Complete triangle.
+5. Add width parameter.
+"""
+
 from turtle import *
+import turtle
+
 from freegames import vector
 
+
 def line(start, end):
-    "Draw line from start to end."
+    """Draw line from start to end."""
     up()
     goto(start.x, start.y)
     down()
     goto(end.x, end.y)
 
+
 def square(start, end):
-    "Draw square from start to end."
+    """Draw square from start to end."""
     up()
     goto(start.x, start.y)
     down()
@@ -21,20 +36,29 @@ def square(start, end):
 
     end_fill()
 
+
 def circle(start, end):
-    "Draw circle from start to end."
-    pass  # TODO
+    """Draw circle from start to end."""
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    turtle.circle(10)
+
 
 def rectangle(start, end):
-    "Draw rectangle from start to end."
+    """Draw rectangle from start to end."""
     pass  # TODO
+
 
 def triangle(start, end):
-    "Draw triangle from start to end."
+    """Draw triangle from start to end."""
     pass  # TODO
 
+
 def tap(x, y):
-    "Store starting point or draw shape."
+    """Store starting point or draw shape."""
     start = state['start']
 
     if start is None:
@@ -45,9 +69,11 @@ def tap(x, y):
         shape(start, end)
         state['start'] = None
 
+
 def store(key, value):
-    "Store value in state at key."
+    """Store value in state at key."""
     state[key] = value
+
 
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
@@ -59,6 +85,10 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+
+# RMEV - 22/03/2022 - We add other color to options
+onkey(lambda: color('yellow'), 'Y')
+
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
